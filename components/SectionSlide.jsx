46927982c694ee4055c773a69e44a4b2085d1a1f
unknown <darielaguilar2012@gmail.com>
2022-08-18 00:06:51 -0400

    Vueltos a poner iconos de redes sociales

--- components/SectionSlide.jsx
+++ components/SectionSlide.jsx
@@ -14,21 +14,21 @@ export default function SectionSlide()
     return(<div className=" flex-1 md:mx-10 mx-2 ">
         <Carousel className="border rounded-lg " activeIndex={index} onSelect={handleSelect}>
             <Carousel.Item className="flex w-100 h-72 sm:h-80">
-                <Image  className="flex  " layout="fill"   src='/dj-mixing.jpg' alt="First Slide"/>
+                <img  className="flex w-100 h-72 sm:h-80 "  src='/dj-mixing.jpg' alt="First Slide"/>
                 <Carousel.Caption>
                     <h3>Sueña</h3>
                     <p className="text-lg">Mientras escuchas nuestra musica</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item className="flex w-100 h-72 sm:h-80">
-                <Image  className="flex  " layout="fill" src='/hitsTrance.png' alt="Second Slide"/>
+                <img  className="flex w-100 h-72 sm:h-80 "  src='/hitsTrance.png' alt="Second Slide"/>
                 <Carousel.Caption>
                     
                     <p className="text-lg">Siente el uplifting y salta</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item className="flex w-100 h-72 sm:h-80">
-                <Image className="flex  " layout="fill" src='/tranceLight.jpg'  alt="Third Slide"/>
+                <img className="flex w-100 h-72 sm:h-80 "  src='/tranceLight.jpg'  alt="Third Slide"/>
                 <Carousel.Caption>
                     <h3>Crea</h3>
                     <p className="text-gray-300 text-lg">Aprende con nosotros e ilumina al mundo con tu potencial</p>
--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
   <Component {...pageProps} />
   </>)
 }
