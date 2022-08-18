597669dc47e66a7d37aa396ba30fe5a3cdee114c
unknown <darielaguilar2012@gmail.com>
2022-08-17 23:49:52 -0400

    Optimizando

--- components/SectionSlide.jsx
+++ components/SectionSlide.jsx
@@ -12,23 +12,23 @@ export default function SectionSlide()
     }
    
     return(<div className=" flex-1 md:mx-10 mx-2 ">
-        <Carousel className="border rounded-lg w-100 h-72" activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item className="flex w-100 h-72 ">
-                <Image  className="flex w-100 h-72 " layout="fill"   src='/dj-mixing.jpg' alt="First Slide"/>
+        <Carousel className="border rounded-lg " activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item className="flex w-100 h-72 sm:h-80">
+                <Image  className="flex  " layout="fill"   src='/dj-mixing.jpg' alt="First Slide"/>
                 <Carousel.Caption>
                     <h3>Sueña</h3>
-                    <p>Mientras escuchas nuestra musica</p>
+                    <p className="text-lg">Mientras escuchas nuestra musica</p>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item className="flex w-100 h-72 ">
-                <Image  className="flex  w-100 h-72 sm:w-100 sm:h-96" layout="fill" src='/hitsTrance.png' alt="Second Slide"/>
+            <Carousel.Item className="flex w-100 h-72 sm:h-80">
+                <Image  className="flex  " layout="fill" src='/hitsTrance.png' alt="Second Slide"/>
                 <Carousel.Caption>
                     
-                    <p>Siente el uplifting y salta</p>
+                    <p className="text-lg">Siente el uplifting y salta</p>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item className="flex w-100 h-72 ">
-                <Image className="flex  w-100 h-72 sm:w-100 sm:h-96" layout="fill" src='/tranceLight.jpg'  alt="Third Slide"/>
+            <Carousel.Item className="flex w-100 h-72 sm:h-80">
+                <Image className="flex  " layout="fill" src='/tranceLight.jpg'  alt="Third Slide"/>
                 <Carousel.Caption>
                     <h3>Crea</h3>
                     <p className="text-gray-300 text-lg">Aprende con nosotros e ilumina al mundo con tu potencial</p>
